7b3c806cbb12f9521d3ecae283bd247cace29215
gnobu <inyangubong17@gmail.com>
2022-09-12 10:37:01 +0100

    initialised message route

---
+++ routes/message.route.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+// const { protect } = require('../middleware/authMiddleware');
+// const { sendMessage, allMessages } = require('../controllers/messageControllers');
+
+// router.post('/', protect, sendMessage);
+// router.get('/:chatId', protect, allMessages);
+
+module.exports = router;
